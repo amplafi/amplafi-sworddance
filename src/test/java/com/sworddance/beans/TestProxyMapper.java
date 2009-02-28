743872b81a7449605e5599e06d4d96d2b5fef372
Patrick Moore <transparentpolitics@gmail.com>
2009-02-28 07:19:46 +0000

    more ProxyMapper work for perf work
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-sworddance/trunk@190 6eaf30a2-012b-4626-ba8e-69dab18af4f2

--- src/test/java/com/sworddance/beans/TestProxyMapper.java
+++ src/test/java/com/sworddance/beans/TestProxyMapper.java
@@ -6,6 +6,8 @@ package com.sworddance.beans;
 
 import static org.testng.Assert.*;
 
+import java.lang.reflect.Proxy;
+
 import org.testng.annotations.Test;
 /**
  * @author patmoore
@@ -13,15 +15,31 @@ import org.testng.annotations.Test;
  */
 public class TestProxyMapper {
 
-    @Test(enabled=false)
+    @Test
     public void testSimpleGetSet() {
-        Interface1Impl child = new Interface1Impl(false, null);
-        Interface1Impl impl = new Interface1Impl(true, child);
-        Interface1 interface1 = ProxyMapper.getProxy(impl, "goo", "child.goo");
+        Interface1Impl child1 = new Interface1Impl(2, true, null);
+        Interface1Impl child = new Interface1Impl(1, false, child1);
+        Interface1Impl impl = new Interface1Impl(0, true, child);
+        Interface1 interface1 = ProxyFactoryImpl.getProxy(impl, "goo", "child.goo");
+        // make sure we are not looking at the original objects
+        testProxyUnique(interface1, child1, child, impl);
+
         Interface1 returnedChild = interface1.getChild();
-        assertNotNull(returnedChild);
-        assertNotSame(child, returnedChild);
+        testProxyUnique(returnedChild, interface1, child1, child, impl);
         assertFalse(returnedChild.isGoo());
+
+        Interface1 returnedChild1 = returnedChild.getChild();
+        testProxyUnique(returnedChild1, returnedChild, interface1, child1, child, impl);
+        assertSame(child1, ProxyMapper.getRealObject(returnedChild1));
+        assertTrue(returnedChild1.isGoo());
+    }
+
+    private void testProxyUnique(Object value, Object...others) {
+        assertNotNull(value);
+        assertTrue(Proxy.isProxyClass(value.getClass()));
+        for (Object other: others) {
+            assertNotSame(value, other);
+        }
     }
 
     public static interface Interface1 {
@@ -34,9 +52,11 @@ public class TestProxyMapper {
     public static class Interface1Impl implements Interface1 {
         private Interface1 child;
         private boolean goo;
-        public Interface1Impl(boolean goo, Interface1Impl child) {
+        private int lvl;
+        public Interface1Impl(int lvl, boolean goo, Interface1Impl child) {
             this.goo = goo;
             this.child = child;
+            this.setLvl(lvl);
         }
         /**
          *
@@ -66,6 +86,18 @@ public class TestProxyMapper {
          */
         public boolean isGoo() {
             return goo;
+        }
+        /**
+         * @param lvl the lvl to set
+         */
+        public void setLvl(int lvl) {
+            this.lvl = lvl;
+        }
+        /**
+         * @return the lvl
+         */
+        public int getLvl() {
+            return lvl;
         }
     }
 }
