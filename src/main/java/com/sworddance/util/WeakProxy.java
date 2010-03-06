d8d22fb5b50af5cf715dc41dbec3f571cca7af59
patmoore <patmoore@amplafi.com>
2010-03-06 02:12:45 +0000

    now the FlowDefinitionProperty and its FPVP, change listener, FlowTranslator are all wired up with services. This allows automatic reloading of entities in a cleaner way
    Also got rid of FlowPropertyDefinition.isRequired() didn't seem to have any use any more.
    
    Created LanguageFlowPropertyValueProvider to help test this out.
    
    
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-sworddance/trunk@626 6eaf30a2-012b-4626-ba8e-69dab18af4f2

--- src/main/java/com/sworddance/util/WeakProxy.java
+++ src/main/java/com/sworddance/util/WeakProxy.java
@@ -36,25 +36,62 @@ public class WeakProxy {
      * @return null if referent == null otherwise returns a proxy implementing the interfaces
      */
     @SuppressWarnings("unchecked")
-    public static <T> T newProxyInstance(final T referent, Class<?>...interfaces) {
+    public static <T> T newProxyInstance(final Object referent, Class<?>...interfaces) {
         if ( referent == null ) {
             return null;
         } else {
+            Object actualReferent = getActual(referent);
             Class<?> clazz = getFirst(interfaces);
             if ( clazz == null) {
-                clazz = referent.getClass();
+                clazz = actualReferent.getClass();
                 interfaces = clazz.getInterfaces();
             }
-            T t = (T) Proxy.newProxyInstance(clazz.getClassLoader(), interfaces, new InvocationHandler() {
-
-                final WeakReference<T> objectRef = new WeakReference<T>(referent);
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    T actual = objectRef.get();
-                    return method.invoke(actual, args);
-                }
-            });
+            T t = (T) Proxy.newProxyInstance(clazz.getClassLoader(), interfaces, new ProxyInvocationHandler<T>(actualReferent));
             return t;
+        }
+    }
+
+    public static boolean isWired(Object proxy) {
+        if ( proxy != null && Proxy.isProxyClass(proxy.getClass())){
+            InvocationHandler invocationHandler = Proxy.getInvocationHandler(proxy);
+            if ( invocationHandler instanceof ProxyInvocationHandler<?>) {
+                return ((ProxyInvocationHandler<?>)invocationHandler).isWired();
+            }
+        }
+        return false;
+    }
+    @SuppressWarnings("unchecked")
+    public static <T> T getActual(Object proxy) {
+        if ( proxy != null && Proxy.isProxyClass(proxy.getClass())){
+            InvocationHandler invocationHandler = Proxy.getInvocationHandler(proxy);
+            if ( invocationHandler instanceof ProxyInvocationHandler<?>) {
+                return (T) ((ProxyInvocationHandler<?>)invocationHandler).getActual();
+            }
+        }
+        return (T) proxy;
+    }
+    protected static class ProxyInvocationHandler<T> implements InvocationHandler {
+        private final WeakReference<T> objectRef;
+        protected ProxyInvocationHandler(Object referent) {
+            objectRef = new WeakReference<T>((T)referent);
+        }
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if ( isWired() ) {
+                T actual = getActual();
+                return method.invoke(actual, args);
+            } else {
+                throw new ApplicationNullPointerException("weakreference was dropped");
+            }
+        }
+        /**
+         * @return the objectRef
+         */
+        public T getActual() {
+            return objectRef.get();
+        }
+        public boolean isWired() {
+            return objectRef != null && getActual() != null;
         }
     }
 }
