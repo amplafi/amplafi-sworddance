6fad8e4ee762b45f9f6f245addf5f7703129fa01
Patrick Moore <patmoore@amplafi.com>
2011-02-22 21:24:48 -0800

    createTracking now has better selection mechanism

--- src/main/java/com/sworddance/util/CreateTracking.java
+++ src/main/java/com/sworddance/util/CreateTracking.java
@@ -18,9 +18,14 @@ import java.io.PrintWriter;
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
 
 import static org.apache.commons.lang.StringUtils.*;
 
@@ -30,18 +35,25 @@ import static org.apache.commons.lang.StringUtils.*;
  * @author Patrick Moore
  */
 public class CreateTracking implements Serializable {
-    private static final boolean ENABLE;
-    private static final List<String> classNames;
+    /**
+     * set to a comma separated list of class names. Class names may be full class names, regex or just the simple class names ( class name without the package ).
+     *
+     * If set to true then all objects are tracked.
+     */
+    public static final String CREATE_TRACKING_SYSTEM_PROPERTY = "create-tracking";
+    private static boolean CREATE_TRACK_ALL;
+    /**
+     * Set by "create-tracking" system property.
+     */
+    private static final List<String> classNames = new ArrayList<String>();
+    private static Pattern classNameMatching;
+    private static final Map<Class, Boolean> createTracking = new ConcurrentHashMap<Class,Boolean>();
     private String createStr;
     private Exception e;
+
     static {
-        String property = System.getProperty("create-tracking");
-        ENABLE = Boolean.parseBoolean(property);
-        classNames = new ArrayList<String>();
-        if ( !ENABLE && isNotBlank(property)) {
-            String[] classNamesArr = property.split("\\s*,\\s*");
-            Collections.addAll(classNames,classNamesArr);
-        }
+        String property = System.getProperty(CREATE_TRACKING_SYSTEM_PROPERTY);
+        initialize(property);
     }
     public CreateTracking() {
         e = new Exception(new Date().toString());
@@ -55,10 +67,16 @@ public class CreateTracking implements Serializable {
     }
     public static CreateTracking getInstance(Boolean create, Class<?> clazz) {
         if ( create == null ) {
-            create = ENABLE;
+            create = CREATE_TRACK_ALL;
         }
-        if ( !create && clazz != null) {
-            create = classNames.contains( clazz.getName());
+        if ( !create && clazz != null && classNameMatching != null) {
+            create = createTracking.get(clazz);
+            if ( create == null ) {
+                // do not yet know for this class
+                Matcher matcher = classNameMatching.matcher(clazz.getName());
+                create = Boolean.valueOf(matcher.find());
+                createTracking.put(clazz, create);
+            }
         }
         return create?new CreateTracking():null;
     }
@@ -71,5 +89,29 @@ public class CreateTracking implements Serializable {
             this.createStr = sw.toString();
         }
         return this.createStr;
+    }
+    public static void initialize(String propertySetting) {
+        CREATE_TRACK_ALL = Boolean.parseBoolean(propertySetting);
+        classNames.clear();
+        createTracking.clear();
+        classNameMatching = null;
+        if ( !CREATE_TRACK_ALL && isNotBlank(propertySetting)) {
+            StringBuilder patternBuilder = new StringBuilder();
+            String[] classNamesArr = propertySetting.split(",");
+            for(String className: NotNullIterator.<String>newNotNullIterator(classNamesArr)) {
+                if ( StringUtils.isNotBlank(className)) {
+                    String trimmedClassName = className.trim();
+                    if ( trimmedClassName.indexOf('.') < 0 ) {
+                        // no package name so probably simple name.
+                        // therefore look for any package. ( leading . or $ for inner class )
+                        patternBuilder.append("(?:.*(?:\\.|\\$))").append(trimmedClassName);
+                        patternBuilder.append("|");
+                    }
+                    patternBuilder.append(trimmedClassName);
+                    classNames.add(trimmedClassName);
+                }
+            }
+            classNameMatching = Pattern.compile(patternBuilder.toString());
+        }
     }
 }
